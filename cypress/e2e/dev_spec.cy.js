8f69ddf9f93c47843b3ec1e5d2bafa1f2ce9c892
Roy Marcelino <rcomarceli@gmail.com>
2023-02-13 21:54:16 -0500

    Github action workflow changes to support dev CICD

--- cypress/e2e/dev_spec.cy.js
+++ cypress/e2e/dev_spec.cy.js
@@ -20,25 +20,3 @@ describe('Checking Dev website backend', () => {
 
   })
 })
-
-describe('Checking Prod website backend', () => {
-  it('increments the visitor counter', () => {
-    
-      cy.visit(Cypress.env('PROD_DOMAIN')); // change URL to match your dev URL
-
-
-
-      cy.get('[data-cy="visitorCount"]').contains(/\d+/).then(($count) =>{
-          const count1 = parseInt($count.text());
-          console.log(count1);
-          cy.window().its('API_URL').then(url => {
-              // cy.request('POST', url);
-              // cy.request('GET', url).its('body').then(text => parseInt(text)).should('be.gt', count1);
-              cy.request('POST', url).its('body').then(text => parseInt(text)).should('be.gt', count1);
-          })
-
-      });
-
-
-  })
-})
